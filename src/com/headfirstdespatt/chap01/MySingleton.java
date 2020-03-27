902efddd2a24be2162429e8fcb179653979fde63
sivajik <sivaji_kondapalli@yahoo.com>
2020-03-27 11:15:23 +0530

    Bill Pugh Method

---
+++ src/com/headfirstdespatt/chap01/MySingleton.java
@@ -0,0 +1,34 @@
+package com.headfirstdespatt.chap01;
+
+public class MySingleton {
+	private static volatile MySingleton myUniqInstance /*= new MySingleton()*/;
+
+	private MySingleton() {
+
+	}
+
+	public /*synchronized*/ MySingleton getInstance() {
+		/*if (myUniqInstance == null) {
+			myUniqInstance = new MySingleton();
+		}*/
+		
+		// return LazyLoader.INSTANCE; // Bill Pugh Method.
+		return myUniqInstance;
+	}
+	
+	public MySingleton smartwayGetInstance() {
+		if (myUniqInstance == null) {
+			synchronized (MySingleton.class) {
+				if (myUniqInstance == null) {
+					myUniqInstance = new MySingleton();
+				}
+			}
+		}
+		return myUniqInstance;
+	}
+	
+	
+	private static class LazyLoader {
+		private static final MySingleton INSTANCE = new MySingleton();
+	}
+}
