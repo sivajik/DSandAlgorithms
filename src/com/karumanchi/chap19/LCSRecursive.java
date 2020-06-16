f9d631fef93870f0e5a4f81e2aae0e09bd36d0c6
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-16 09:45:16 +0530

    LCSRecursive

--- src/com/karumanchi/chap19/LCSRecursive.java
+++ src/com/karumanchi/chap19/LCSRecursive.java
@@ -3,21 +3,21 @@ package com.karumanchi.chap19;
 public class LCSRecursive {
 
 	public static void main(String[] args) {
-		System.out.println(LCSRecursive("ABCBDAB", "BDCABA"));
+		System.out.println(LCSRecursiveApproach("ABCBDAB", "BDCABA"));
 
 	}
 
-	static public String LCSRecursive(String x, String y) {
+	static public String LCSRecursiveApproach(String x, String y) {
 		int m = x.length();
 		int n = y.length();
 
 		if (m == 0 || n == 0) {
 			return "";
 		} else if (x.charAt(m - 1) == y.charAt(n - 1)) {
-			return LCSRecursive(x.substring(0, m - 1), y.substring(0, n - 1)) + x.charAt(m - 1);
+			return LCSRecursiveApproach(x.substring(0, m - 1), y.substring(0, n - 1)) + x.charAt(m - 1);
 		} else {
-			String p = LCSRecursive(x.substring(0, m - 1), y);
-			String q = LCSRecursive(x, y.substring(0, n - 1));
+			String p = LCSRecursiveApproach(x.substring(0, m - 1), y);
+			String q = LCSRecursiveApproach(x, y.substring(0, n - 1));
 			if (p.length() > q.length()) {
 				return p;
 			} else {
