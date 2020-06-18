1a3f5c8dfc860d7da22be99ca0000cc1231d01cb
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-18 06:18:48 +0530

    Prob20_LongestIncreasingSequence_LIS

---
+++ src/com/karumanchi/chap19/Prob20_LongestIncreasingSequence_LIS.java
@@ -0,0 +1,29 @@
+package com.karumanchi.chap19;
+
+import java.util.Arrays;
+
+public class Prob20_LongestIncreasingSequence_LIS {
+
+	public static void main(String[] args) {
+		System.out.println(LIS(new int[] { 5, 6, 2, 3, 4, 1, 9, 9, 8, 9, 5 }));
+	}
+
+	static public int LIS(int[] a) {
+		int[] lisTable = new int[a.length + 1];
+
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (a[i] > a[j] && lisTable[i] < lisTable[j] + 1) {
+					lisTable[i] = lisTable[j] + 1;
+				}
+			}
+		}
+
+		System.out.println(Arrays.toString(lisTable));
+		int max = Integer.MIN_VALUE;
+		for (int elem : lisTable) {
+			max = Math.max(elem, max);
+		}
+		return max;
+	}
+}
