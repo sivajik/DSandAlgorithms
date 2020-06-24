e953c465efcaf8d1dc084735130d20cf9dc68dcc
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-24 10:04:00 +0530

    Prob32_LCS

--- src/com/gfg/algorithms/dp/basic/Prob010_FriendsPairing.java
+++ src/com/gfg/algorithms/dp/basic/Prob010_FriendsPairing.java
@@ -5,6 +5,7 @@ public class Prob010_FriendsPairing {
 	public static void main(String[] args) {
 		System.out.println(friendsPairing(3));
 		System.out.println(friendsPairing(5));
+		System.out.println(friendsPairing(2));
 	}
 
 	static int friendsPairing(int n) {
@@ -13,7 +14,7 @@ public class Prob010_FriendsPairing {
 		dp[0] = 0; // no friends, no pairing
 		dp[1] = 1; // lone ranger, 1 possibilty.
 		dp[2] = 2; // 2 people, option 1 -> both separate option 2 -> paired as one group.
-		for (int i = 2; i <= n; i++) {
+		for (int i = 3; i <= n; i++) {
 			dp[i] = dp[i - 1] + (dp[i - 2] * (i - 1));
 		}
 		return dp[n];
