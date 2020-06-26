929b3a05dc1a8a080fd8146642d89e59cc62d9a7
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-26 06:36:45 +0530

    Prob18_RodCutting

---
+++ src/com/gfg/algorithms/dp/basic/Prob18_RodCutting.java
@@ -0,0 +1,26 @@
+package com.gfg.algorithms.dp.basic;
+
+public class Prob18_RodCutting {
+
+	public static void main(String[] args) {
+		int arr[] = new int[] { 1, 5, 8, 9, 10, 17, 17, 20 };
+		System.out.println("Maximum Obtainable Value is " + cutRod(arr));
+
+	}
+
+	private static int cutRod(int[] arr) {
+		int max = Integer.MIN_VALUE;
+		int[] dp = new int[arr.length + 1];
+		
+		
+		for (int i = 1; i <= arr.length; i++) {
+			for (int j = 0; j < i; j++) {
+				max = Math.max(max, arr[j] + dp[i-j-1]);
+			}
+			dp[i] = max;
+		}
+		// TODO Auto-generated method stub
+		return dp[arr.length];
+	}
+
+}
