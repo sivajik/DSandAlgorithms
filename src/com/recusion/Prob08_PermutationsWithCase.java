0dbf43885a624d043ca3c3867adc07d2bb9f676f
sivajik <sivaji_kondapalli@yahoo.com>
2020-08-18 23:46:52 +0530

    Prob08_PermutationsWithCase

---
+++ src/com/recusion/Prob08_PermutationsWithCase.java
@@ -0,0 +1,29 @@
+package com.recusion;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Prob08_PermutationsWithCase {
+
+	public static void main(String[] args) {
+		Set<String> res = new HashSet<>();
+		helper("abc", "", res);
+		for (String s : res) {
+			System.out.println(s);
+		}
+	}
+
+	private static void helper(String ip, String op, Set<String> res) {
+		if (0 == ip.length()) {
+			res.add(op);
+			return;
+		}
+
+
+		String op1 = op + Character.toUpperCase(ip.charAt(0));
+		String op2 = op + Character.toLowerCase(ip.charAt(0));
+		ip = ip.substring(1);
+		helper(ip, op2, res);
+		helper(ip, op1, res);
+	}
+}
