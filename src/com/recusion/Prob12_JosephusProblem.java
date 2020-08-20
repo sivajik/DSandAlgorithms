3ab07d48f1efd811c8bc10c9dc73e357cec3ec3f
sivajik <sivaji_kondapalli@yahoo.com>
2020-08-20 23:39:32 +0530

    Prob12_JosephusProblem

---
+++ src/com/recusion/Prob12_JosephusProblem.java
@@ -0,0 +1,30 @@
+package com.recusion;
+
+import java.util.Arrays;
+
+public class Prob12_JosephusProblem {
+
+	public static void main(String[] args) {
+		int n = 40;
+		int k = 7;
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = i + 1;
+		}
+		System.out.println(solve(arr, k - 1, 0));
+
+	}
+
+	public static int solve(int[] arr, int k, int startAt) {
+		if (arr.length == 1) {
+			return arr[0];
+		}
+		int positionToPlace = (startAt + k) % arr.length;
+		for (int i = positionToPlace; i < arr.length-1; i++) {
+			arr[i] = arr[i+1]; 
+		}
+		arr = Arrays.copyOf(arr, arr.length-1);
+		return solve(arr, k, positionToPlace);
+	}
+}
