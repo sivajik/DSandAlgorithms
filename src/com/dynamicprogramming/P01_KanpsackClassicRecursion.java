b2da9b5da5d2f4d84037736065a59225afc04af1
sivajik <sivaji_kondapalli@yahoo.com>
2020-08-31 11:26:19 +0530

    Classic Recusion Knapsack

---
+++ src/com/dynamicprogramming/P01_KanpsackClassicRecursion.java
@@ -0,0 +1,22 @@
+package com.dynamicprogramming;
+
+public class P01_KanpsackClassicRecursion {
+
+	public static void main(String[] args) {
+		System.out.println(maxProfit(new int[] { 1, 3, 4, 5 }, new int[] { 1, 4, 5, 7 }, 10, 4));
+	}
+
+	public static int maxProfit(int[] wt, int[] val, int W, int n) {
+		if (W == 0 || n == 0) {
+			return 0;
+		}
+
+		if (wt[n - 1] <= W) {
+			int includeCase = val[n - 1] + maxProfit(wt, val, W - wt[n - 1], n - 1);
+			int excludeCase = maxProfit(wt, val, W - wt[n - 1], n - 1);
+			return Math.max(includeCase, excludeCase);
+		} else {
+			return maxProfit(wt, val, W - wt[n - 1], n - 1);
+		}
+	}
+}
