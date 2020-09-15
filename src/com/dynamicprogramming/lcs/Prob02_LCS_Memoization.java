2b65783da4750f5d04ec2e3d5691590b08b8f466
sivajik <sivaji_kondapalli@yahoo.com>
2020-09-16 05:06:12 +0530

    Prob02_LCS_Memoization

---
+++ src/com/dynamicprogramming/lcs/Prob02_LCS_Memoization.java
@@ -0,0 +1,47 @@
+package com.dynamicprogramming.lcs;
+
+public class Prob02_LCS_Memoization {
+
+	public static void main(String[] args) {
+		String a = "ABCDGH";
+		String b = "AEDFHR";
+		int[][] matrix = new int[a.length() + 1][b.length() + 1];
+		fill(matrix);
+		System.out.println(lengthOfLCS(a, b, a.length(), b.length(), matrix));
+
+		a = "AGGTAB";
+		b = "GXTXAYB";
+		matrix = new int[a.length() + 1][b.length() + 1];
+		fill(matrix);
+		System.out.println(lengthOfLCS(a, b, a.length(), b.length(), matrix));
+	}
+
+	static int lengthOfLCS(String a, String b, int lenOfA, int lenOfB, int[][] matrix) {
+		if (lenOfA == 0 || lenOfB == 0) {
+			return 0;
+		}
+
+		if (matrix[lenOfA][lenOfA] != -1) {
+			return matrix[lenOfA][lenOfA];
+		}
+		if (a.charAt(lenOfA - 1) == b.charAt(lenOfB - 1)) {
+			return (matrix[lenOfA - 1][lenOfA - 1] = 1 + lengthOfLCS(a, b, lenOfA - 1, lenOfB - 1, matrix));
+		} else {
+			matrix[lenOfA - 1][lenOfB] = lengthOfLCS(a, b, lenOfA - 1, lenOfB, matrix);
+			matrix[lenOfA][lenOfB - 1] = lengthOfLCS(a, b, lenOfA, lenOfB - 1, matrix);
+			return Math.max(matrix[lenOfA - 1][lenOfB], matrix[lenOfA][lenOfB - 1]);
+		}
+	}
+
+	public static void fill(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				if (i == 0 || j == 0) {
+					m[i][j] = 0;
+				} else {
+					m[i][j] = -1;
+				}
+			}
+		}
+	}
+}
