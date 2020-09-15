5b448715302d0d7288c5ae9b6e19ece8e7972f80
sivajik <sivaji_kondapalli@yahoo.com>
2020-09-15 11:06:14 +0530

    Prob01_LCS_Recusion

---
+++ src/com/dynamicprogramming/lcs/Prob01_LCS_Recusion.java
@@ -0,0 +1,22 @@
+package com.dynamicprogramming.lcs;
+
+public class Prob01_LCS_Recusion {
+
+	public static void main(String[] args) {
+		String a = "ABCDGH";
+		String b = "AEDFHR";
+		System.out.println(lengthOfLCS(a, b, a.length(), b.length()));
+	}
+
+	static int lengthOfLCS(String a, String b, int lenOfA, int lenOfB) {
+		if (lenOfA == 0 || lenOfB == 0) {
+			return 0;
+		}
+
+		if (a.charAt(lenOfA - 1) == b.charAt(lenOfB - 1)) {
+			return 1 + lengthOfLCS(a, b, lenOfA - 1, lenOfB - 1);
+		} else {
+			return Math.max(lengthOfLCS(a, b, lenOfA - 1, lenOfB), lengthOfLCS(a, b, lenOfA, lenOfB - 1));
+		}
+	}
+}
