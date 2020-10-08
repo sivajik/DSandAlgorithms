76047a46c658ac653d9929c6f52afadbc4701b5d
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-08 20:54:13 +0100

    modifed

--- src/com/crackingthetcodingtinterviews/chapter03/Prob_03_01.java
+++ src/com/crackingthetcodingtinterviews/chapter03/Prob_03_01.java
@@ -2,7 +2,22 @@ package com.crackingthetcodingtinterviews.chapter03;
 
 public class Prob_03_01 {
 	public static void main(String[] args) throws Exception {
-
+		FixedMultiStack f = new FixedMultiStack(10);
+		f.push(0, 20);
+		f.push(0, 21);
+		f.push(0, 21);
+		f.push(0, 22);
+		
+		f.push(1, 31);
+		f.push(1, 32);
+		
+		f.push(2, 41);
+		f.push(2, 42);
+		f.push(2, 43);
+		
+		System.out.println(f.pop(0));
+		System.out.println(f.pop(1));
+		System.out.println(f.pop(2));
 	}
 }
 
