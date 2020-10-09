65e89e2089dfc8668908dd1a6d6565cc75c231f7
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-09 21:26:33 +0100

    Kth Largest

---
+++ src/com/gfg/ds/heap/Prob01_02.java
@@ -0,0 +1,23 @@
+package com.gfg.ds.heap;
+
+import java.util.PriorityQueue;
+
+public class Prob01_02 {
+	// Kth largest (means make MinHeap - by default its min heap)
+	public static void main(String[] args) {
+		PriorityQueue<Integer> p = new PriorityQueue<>();
+
+		int[] arr = new int[] { 7, 10, 4, 3, 20, 15 };
+		int k = 3;
+		int cnt = 0;
+		for (; cnt < arr.length; cnt++) {
+			p.add(arr[cnt]);
+
+			if (p.size() > k) {
+				p.poll();
+			}
+		}
+		System.out.println(p.peek());
+	}
+
+}
