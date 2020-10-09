a88a824747b5916abfa91dc7849fd8ab2bd53faf
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-09 22:10:35 +0100

    Prob01_03_KSortedArray

--- src/com/gfg/ds/heap/Prob01_03_KSortedArray.java
+++ src/com/gfg/ds/heap/Prob01_03_KSortedArray.java
@@ -11,11 +11,25 @@ public class Prob01_03_KSortedArray {
 
 		int[] arr = new int[] { 6, 5, 3, 2, 8, 10, 9 };
 		int k = 3;
+		List<Integer> l = new ArrayList<>();
+
+		for (int i : arr) {
+			p.add(i);
+			if (p.size() > k) {
+				l.add(p.poll());
+			}
+		}
+		while (!p.isEmpty()) {
+			l.add(p.poll());
+		}
+		System.out.println(l);
+		
+		System.out.println("---- BS approach -----");
+		l = new ArrayList<>();
 
 		for (int i = 0; i < k + 1; i++) {
 			p.add(arr[i]);
 		}
-		List<Integer> l = new ArrayList<>();
 		for (int i = k + 1; i < arr.length; i++) {
 			l.add(p.poll());
 			p.add(arr[i]);
