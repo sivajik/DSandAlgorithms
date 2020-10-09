f0bf8ce08b80f2e44c537e68d96341d65935b50e
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-09 21:27:19 +0100

    renamed

--- src/com/gfg/ds/heap/Prob01_02_KthLargest.java
+++ src/com/gfg/ds/heap/Prob01_02_KthLargest.java
@@ -2,7 +2,7 @@ package com.gfg.ds.heap;
 
 import java.util.PriorityQueue;
 
-public class Prob01_02 {
+public class Prob01_02_KthLargest {
 	// Kth largest (means make MinHeap - by default its min heap)
 	public static void main(String[] args) {
 		PriorityQueue<Integer> p = new PriorityQueue<>();
