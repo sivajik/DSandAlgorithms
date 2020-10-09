d29b31c1378da8e04f4f47e489b337a0629b1724
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-09 21:59:46 +0100

    print all

--- src/com/gfg/ds/heap/Prob01_02_KthLargest.java
+++ src/com/gfg/ds/heap/Prob01_02_KthLargest.java
@@ -1,5 +1,7 @@
 package com.gfg.ds.heap;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Prob01_02_KthLargest {
@@ -10,6 +12,8 @@ public class Prob01_02_KthLargest {
 		int[] arr = new int[] { 7, 10, 4, 3, 20, 15 };
 		int k = 3;
 		int cnt = 0;
+
+		List<Integer> ans = new ArrayList<>();
 		for (; cnt < arr.length; cnt++) {
 			p.add(arr[cnt]);
 
@@ -17,7 +21,10 @@ public class Prob01_02_KthLargest {
 				p.poll();
 			}
 		}
-		System.out.println(p.peek());
+		while (!p.isEmpty()) {
+			ans.add(p.poll());
+		}
+		System.out.println(ans);
 	}
 
 }
