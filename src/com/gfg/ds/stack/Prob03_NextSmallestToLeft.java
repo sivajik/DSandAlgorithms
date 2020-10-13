ec21eed2a1250ad102b3d09b7edd07db52f2cc9c
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-13 19:53:55 +0100

    Next Smaller to Left

---
+++ src/com/gfg/ds/stack/Prob03_NextSmallestToLeft.java
@@ -0,0 +1,39 @@
+package com.gfg.ds.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Prob03_NextSmallestToLeft {
+	// Try with 1 3 2 4 and a picture it on paper.
+	public static void main(String[] args) {
+		int[] arr = new int[] { 4, 5, 2, 10, 8 };
+
+		Stack<Integer> s = new Stack<Integer>();
+		List<Integer> result = new ArrayList<>();
+
+		// change 1 : for (int i = arr.length - 1; i >= 0; i--) {
+		for (int i = 0; i < arr.length; i++) {
+			if (s.isEmpty()) {
+				result.add(-1);
+			} else if (s.size() > 0 && s.peek() < arr[i]) {
+				result.add(s.peek());
+			} else if (s.size() > 0 && s.peek() >= arr[i]) {
+
+				while (s.size() > 0 && s.peek() >= arr[i]) {
+					s.pop();
+				}
+				if (s.size() == 0) {
+					result.add(-1);
+				} else {
+					result.add(s.peek());
+				}
+			}
+			s.push(arr[i]);
+		}
+		// change 2: no need to reverse
+		// Collections.reverse(result);
+		System.out.println(result);
+	}
+
+}
