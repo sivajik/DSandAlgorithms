26da6e46f2c5680686638e09450edb420501be1b
sivajik <sivaji_kondapalli@yahoo.com>
2021-01-18 21:59:47 +0000

    ZeroOneKnapsackProblem

---
+++ src/com/dynamicprogramming/revision/ZeroOneKnapsackProblem.java
@@ -0,0 +1,31 @@
+package com.dynamicprogramming.revision;
+
+public class ZeroOneKnapsackProblem {
+
+	public static void main(String[] args) {
+		int W = 50;
+		int N = 3;
+
+		int[][] dp = new int[1 + N][1 + W];
+
+		int[] wt = new int[] { 10, 20, 30 };
+		int[] val = new int[] { 60, 100, 120 };
+		// i : N
+		// j: Weights
+		for (int i = 0; i < N + 1; i++) {
+			for (int j = 0; j < W + 1; j++) {
+				if (i == 0 || j == 0) {
+					dp[i][j] = 0;
+				} else {
+					if (wt[i - 1] <= j) {
+						dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+					} else {
+						dp[i][j] = dp[i - 1][j];
+					}
+				}
+			}
+		}
+		System.out.println(dp[N][W]);
+	}
+	// val[n-1] + knapsack(vals, wt, W-wt[n-1], n-1)
+}
