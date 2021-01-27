7138a4ed0bbb45aa6d9d22f510cf68b5440d14d7
sivajik <sivaji_kondapalli@yahoo.com>
2021-01-27 21:12:08 +0000

    Prob78

---
+++ src/com/gfg/algorithms/backtracking/leetcodepatterns/Prob78.java
@@ -0,0 +1,29 @@
+package com.gfg.algorithms.backtracking.leetcodepatterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prob78 {
+
+	public static void main(String[] args) {
+		for (List<Integer> l : subsets(new int[] { 1, 2, 3 })) {
+			System.out.println(l);
+		}
+	}
+
+	static public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		helper(nums, 0, new ArrayList<>(), res);
+		return res;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void helper(int[] nums, int currIndex, ArrayList<Integer> tempList, List<List<Integer>> res) {
+		res.add(new ArrayList(tempList));
+		for (int i = currIndex; i < nums.length; i++) {
+			tempList.add(nums[i]);
+			helper(nums, i + 1, tempList, res);
+			tempList.remove(tempList.size() - 1);
+		}
+	}
+}
