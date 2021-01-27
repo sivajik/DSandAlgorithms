71e09f3e64b4095fca4039f0191fabf5931899c0
sivajik <sivaji_kondapalli@yahoo.com>
2021-01-27 21:55:21 +0000

    d

--- src/com/gfg/algorithms/backtracking/leetcodepatterns/Prob40.java
+++ src/com/gfg/algorithms/backtracking/leetcodepatterns/Prob40.java
@@ -1,39 +1,33 @@
 package com.gfg.algorithms.backtracking.leetcodepatterns;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Prob39 {
+public class Prob40 {
 
 	public static void main(String[] args) {
-		for (List<Integer> l : combinationSum(new int[] { 10, 1, 2, 7, 6, 1, 5 }, 8)) {
+		for (List<Integer> l : combinationSum(new int[] { 2, 3, 6, 7 }, 7)) {
 			System.out.println(l);
 		}
 	}
 
 	static public List<List<Integer>> combinationSum(int[] nums, int target) {
 		List<List<Integer>> res = new ArrayList<>();
-		Arrays.sort(nums);
 		helper(nums, new ArrayList<>(), res, 0, target);
 		return res;
 	}
 
 	private static void helper(int[] nums, ArrayList temp, List<List<Integer>> res, int start, int target) {
-		if (target < 0) {
-			return;
-		}
 		if (target == 0) {
 			res.add(new ArrayList<>(temp));
 			return;
 		}
 		for (int i = start; i < nums.length; i++) {
-			if (i > start && nums[i] == nums[i - 1]) {
-				continue;
+			if (nums[i] <= target) {
+				temp.add(nums[i]);
+				helper(nums, temp, res, i, target - nums[i]);
+				temp.remove(temp.size() - 1);
 			}
-			temp.add(nums[i]);
-			helper(nums, temp, res, i + 1, target - nums[i]);
-			temp.remove(temp.size() - 1);
 		}
 	}
 
