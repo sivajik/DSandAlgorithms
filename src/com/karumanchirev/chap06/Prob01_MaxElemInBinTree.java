2246ccd6ac2246d6717a8bb1e121ab1221f7cf41
sivajik <sivaji_kondapalli@yahoo.com>
2022-05-12 20:49:14 +0100

    shuru huwaaa

---
+++ src/com/karumanchirev/chap06/Prob01_MaxElemInBinTree.java
@@ -0,0 +1,53 @@
+package com.karumanchirev.chap06;
+
+public class Prob01_MaxElemInBinTree {
+
+	public static void main(String[] args) {
+
+		BinaryTreeNode n1 = new BinaryTreeNode(1);
+
+		BinaryTreeNode n2 = new BinaryTreeNode(2);
+		BinaryTreeNode n3 = new BinaryTreeNode(3);
+
+		BinaryTreeNode n4 = new BinaryTreeNode(4);
+		BinaryTreeNode n5 = new BinaryTreeNode(5);
+		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		BinaryTreeNode n7 = new BinaryTreeNode(7);
+
+		n1.left = n2;
+		n1.right = n3;
+
+		n2.left = n4;
+		n2.right = n5;
+
+		n3.left = n6;
+		n3.right = n7;
+
+		int max = n1.maxElement(n1);
+		System.out.println(max);
+	}
+
+	static class BinaryTreeNode {
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+		int value;
+
+		BinaryTreeNode() {
+		}
+
+		public int maxElement(BinaryTreeNode root) {
+			if (root != null) {
+				int lMax = maxElement(root.left);
+				int rMax = maxElement(root.right);
+				return Math.max(root.value, Math.max(lMax, rMax));
+			}
+			return -1;
+		}
+
+		BinaryTreeNode(int x) {
+			this.value = x;
+			this.left = null;
+			this.right = null;
+		}
+	}
+}
