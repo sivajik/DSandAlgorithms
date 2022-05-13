26abf39c7801f931e855859a4a84a783ed34bee4
sivajik <sivaji_kondapalli@yahoo.com>
2022-05-13 21:27:46 +0100

    6,7

---
+++ src/com/karumanchirev/chap06/Prob06_07_SizeBinTree.java
@@ -0,0 +1,96 @@
+package com.karumanchirev.chap06;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Prob06_07_SizeBinTree {
+
+	public static void main(String[] args) {
+
+		BinaryTreeNode n1 = null;
+		for (int i = 1; i <= 15; i++) {
+			n1 = insert(n1, i);
+
+		}
+		System.out.println(n1.sizeRec(n1));
+		System.out.println(n1.sizeItr(n1));
+	}
+
+	static public BinaryTreeNode insert(BinaryTreeNode root, int data) {
+		if (root == null) {
+			return new BinaryTreeNode(data);
+		}
+
+		Queue<BinaryTreeNode> q = new LinkedList<>();
+		q.add(root);
+		while (!q.isEmpty()) {
+			BinaryTreeNode tmp = q.poll();
+			if (tmp != null) {
+				if (tmp.left == null) {
+					tmp.left = new BinaryTreeNode(data);
+					return root;
+				} else {
+					q.add(tmp.left);
+				}
+
+				if (tmp.right == null) {
+					tmp.right = new BinaryTreeNode(data);
+					return root;
+				} else {
+					q.add(tmp.right);
+				}
+			}
+		}
+		return root;
+
+	}
+
+	static class BinaryTreeNode {
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+		int value;
+
+		BinaryTreeNode() {
+		}
+
+		public void inOrder(BinaryTreeNode root) {
+			if (root != null) {
+				inOrder(root.left);
+				System.out.print(root.value + " ");
+				inOrder(root.right);
+			}
+		}
+
+		public int sizeRec(BinaryTreeNode root) {
+			if (root == null) {
+				return 0;
+			}
+			return 1 + sizeRec(root.left) + sizeRec(root.right);
+		}
+
+		public int sizeItr(BinaryTreeNode root) {
+			Queue<BinaryTreeNode> q = new LinkedList<>();
+			q.add(root);
+			int c = 0;
+			while (!q.isEmpty()) {
+				BinaryTreeNode tmp = q.poll();
+				if (tmp != null) {
+					c++;
+					if (tmp.left != null) {
+						q.add(tmp.left);
+					}
+					if (tmp.right != null) {
+						q.add(tmp.right);
+					}
+				}
+			}
+			return c;
+		}
+
+		BinaryTreeNode(int x) {
+			this.value = x;
+			this.left = null;
+			this.right = null;
+		}
+	}
+}
