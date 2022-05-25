a972e6c0b0f6991e0000b66a941ab113a09829fc
sivajik <sivaji_kondapalli@yahoo.com>
2022-05-25 21:37:33 +0100

    Prob25_SumOfAllNodes

---
+++ src/com/karumanchirev/chap06/Prob25_SumOfAllNodes.java
@@ -0,0 +1,73 @@
+package com.karumanchirev.chap06;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Prob25_SumOfAllNodes {
+
+	public static void main(String[] args) {
+
+		BinaryTreeNode n1 = null;
+		for (int i = 1; i <= 15; i++) {
+			n1 = insert(n1, i);
+
+		}
+		System.out.println(n1.sumOfAllNodes(n1));
+	}
+
+	static class BinaryTreeNode {
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+		int value;
+
+		BinaryTreeNode() {
+		}
+
+		public int sumOfAllNodes(BinaryTreeNode root) {
+			if (root ==null) {
+				return 0;
+			} else {
+				return root.value + sumOfAllNodes(root.left) + sumOfAllNodes(root.right);
+			}
+		}
+
+		BinaryTreeNode(int x) {
+			this.value = x;
+			this.left = null;
+			this.right = null;
+		}
+
+		public String toString() {
+			return value + "";
+		}
+
+	}
+
+	static public BinaryTreeNode insert(BinaryTreeNode root, int data) {
+		if (root == null) {
+			return new BinaryTreeNode(data);
+		}
+
+		Queue<BinaryTreeNode> q = new LinkedList<>();
+		q.add(root);
+		while (!q.isEmpty()) {
+			BinaryTreeNode tmp = q.poll();
+			if (tmp != null) {
+				if (tmp.left == null) {
+					tmp.left = new BinaryTreeNode(data);
+					return root;
+				} else {
+					q.add(tmp.left);
+				}
+
+				if (tmp.right == null) {
+					tmp.right = new BinaryTreeNode(data);
+					return root;
+				} else {
+					q.add(tmp.right);
+				}
+			}
+		}
+		return root;
+	}
+}
